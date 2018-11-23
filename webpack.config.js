919527eecece00cccc46ce6256f6bf6b36eff9e8
caiosousa21 <caiofmsousa@gmail.com>
2018-11-23 15:07:36 -0200

    Biblioteca

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+    entry:'./src/index.js',
+    scripts:'',
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename:'webpack-number.js',
+        library:'webpackNumbers',
+        libraryTarget: 'umd'
+    },
+    externals:{
+        lodash:{
+            commonjs:'lodash',
+            commonjs2:'lodash',
+            amd:'lodash',
+            root:'_'
+        }
+    }
+    
+}
